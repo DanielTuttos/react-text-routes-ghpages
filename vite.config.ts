a56dd05b4fcff79dd13db0aaebfd271102742de2
Daniel Romero <tutorialesdanielromero@gmail.com>
2023-11-05 21:37:18 -0500

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://DanielTuttos.github.io/react-text-routes-ghpages'
+  base: "https://DanielTuttos.github.io/react-text-routes-ghpages/"
 })
